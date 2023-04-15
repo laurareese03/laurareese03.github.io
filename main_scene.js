4eb833d0d968da62056f104cc2ff4d9c710b860f
PastelKobold <103293322+PastelKobold@users.noreply.github.com>
2023-04-15 00:05:10 -0400

    Updating main_scene

--- main_scene.js
+++ main_scene.js
@@ -7,52 +7,9 @@ var MainScene = new Phaser.Class({
     },
     preload: function() {
     },
-    createAutoClickers: function() {
-        const forkAC = new AutoClick(1, 1, 1, 1, True);
-        const spoonAC = new AutoClick(1, 1, 1, 1, True);
-        const sporkAC = new AutoClick(1, 1, 1, 1, True);
-        const shovelAC = new AutoClick(1, 1, 1, 1, True);
-        const pickaxeAC = new AutoClick(1, 1, 1, 1, True);
-        const jackhammerAC = new AutoClick(1, 1, 1, 1, True);
-        const drillAC = new AutoClick(1, 1, 1, 1, True);
-        const excavatorAC = new AutoClick(1, 1, 1, 1, True);
-        const cheesemineAC = new AutoClick(1, 1, 1, 1, True);
-    },
-
-    createBuildings: function() {
-        const cardboardboxB = new Building(1, 1);
-        const catCaveB = new Building(1, 1);
-        const shedB = new Building(1, 1);
-        const houseB = new Building(1, 1);
-        const barnB = new Building(1, 1);
-        const studioApartmentComplexB = new Building(1, 1);
-        const multibedApartmentComplexB = new Building(1, 1);
-        const catopiaB = new Building(1, 1);
-    },
-
-    createCatInstants: function() {
-        const treatCI = new CatInstant(1, 1);
-        const treatpileCI = new CatInstant(1, 1);
-        const cheesewheelCI = new CatInstant(1, 1);
-        const catnipCI = new CatInstant(1, 1);
-    }, 
-
-    createCatProgressives: function() {
-        const ballCP = new CatProgressive(1, 1, 1, True);
-        const mouseCP = new CatProgressive(1, 1, 1, True);
-        const ponytailholderCP = new CatProgressive(1, 1, 1, True);
-        const twisttieCP = new CatProgressive(1, 1, 1, True);
-        const laserpointerCP = new CatProgressive(1, 1, 1, True);
-        const cushionCP = new CatProgressive(1, 1, 1, True);
-        const keyboardCP = new CatProgressive(1, 1, 1, True);
-        const scratcherCP = new CatProgressive(1, 1, 1, True);
-        const treeCP = new CatProgressive(1, 1, 1, True);
-    },
     create: function() {
         createAutoClickers();
-        createBuildings();
-        createCatInstants();
-        createCatProgressives();
+        createBuilding();
         var text = this.add.text(
             640, 
             360, 
@@ -69,5 +26,16 @@ var MainScene = new Phaser.Class({
 
 
 
+    createAutoClickers: function() {
+        const forkAC = new AutoClick(1, 1, 1, 1, True);
+        const spoonAC = new AutoClick(1, 1, 1, 1, True);
+        const sporkAC = new AutoClick(1, 1, 1, 1, True);
+        const shovelAC = new AutoClick(1, 1, 1, 1, True);
+        const pickaxeAC = new AutoClick(1, 1, 1, 1, True);
+        const jackhammerAC = new AutoClick(1, 1, 1, 1, True);
+        const drillAC = new AutoClick(1, 1, 1, 1, True);
+        const excavatorAC = new AutoClick(1, 1, 1, 1, True);
+        const cheesemineAC = new AutoClick(1, 1, 1, 1, True);
+    }
 });
 
