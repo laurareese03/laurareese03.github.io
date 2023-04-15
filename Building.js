39d59d7f0c9e1c955142bcbf7f25437a1c0f55e4
Laura Reese <laurareese03@gmail.com>
2023-04-15 13:57:00 -0400

    update building upgrade images

--- Building.js
+++ Building.js
@@ -1,8 +1,8 @@
 // Declaration
 class Building {
-    constructor(cost, cat_limit) {
+    constructor(cost, cat_limit, image) {
       this.cost = cost;
       this.cat_limit = cat_limit;
+      this.image = image;
     }
-  
   }
