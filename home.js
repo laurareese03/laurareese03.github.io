f13a78664b35b7e7d400b9315bf43ae6bdfb8898
RenAYoung <ren.a.y.834@gmail.com>
2023-04-15 01:21:36 -0400

    Adding frame for coding functions later

--- home.js
+++ home.js
@@ -6,10 +6,12 @@ var Home = new Phaser.Class({
     },
     init: function() {},
     preload: function() {
-
+        this.load.image('space_back', 'assets/space_back.webp');
     },
     
     create: function() {
+        this.load.setBaseURL('https://laurareese03.github.io/');
+        this.add.image(640, 360, 'space_back');
         const helloButton = this.add.text(640, 360, 'START', { fill: '#000', fontSize: 50 }).setOrigin(0.5);
         helloButton.setInteractive();
         helloButton.on('pointerdown', () => this.updateScene() );
